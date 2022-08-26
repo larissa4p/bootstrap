c5539e7aefd13740014c34236868acc6d88af535
Larissa <98964013+larissa4p@users.noreply.github.com>
2022-08-26 17:51:49 -0300

    tooltip

--- 9_COMPONENTES/2_aula/js/scripts.js
+++ 9_COMPONENTES/2_aula/js/scripts.js
@@ -3,3 +3,9 @@ var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
+
+var tooltipTriggetList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+
+var tooltipList = tooltipTriggetList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+});
